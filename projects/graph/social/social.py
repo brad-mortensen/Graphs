49a22dd4eea6dc94d5ce4132e9b89e2b48a2a400
Brad Mortensen <b.mort86@gmail.com>
2019-04-10 11:00:40 -0600

    slight social update

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -84,9 +84,10 @@ class SocialGraph:
             if v not in visited:
                 visited[v] = path
                 for friend in self.friendships[v]:
-                    path_copy = list(path)
-                    path_copy.append(friend)
-                    q.enqueue(path_copy)
+                    if friend not in visited:
+                        path_copy = list(path)
+                        path_copy.append(friend)
+                        q.enqueue(path_copy)
         # lengths = []
         # for key, value in visited.items():
         #     lengths.append(len(visited[key]))
